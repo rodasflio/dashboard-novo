1fd75ee1db7787abdae235fe17bc6d1568ce1ac2
rodasflio <askellad4157@gmail.com>
2025-09-24 00:27:53 -0300

    supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jhkpssqjopqorujbjdws.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impoa3Bzc3Fqb3Bxb3J1amJqZHdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NTE2NzMsImV4cCI6MjA3NDIyNzY3M30.QO-Wlyn57byLoyg5M2pKsJ-baOwf-g5QfOzIWuSCw_A';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('As variáveis de ambiente do Supabase não estão configuradas.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
